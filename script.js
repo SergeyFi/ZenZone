f96f47b6a94c112c2706c084dd53506988ae2b38
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2024-07-16 21:49:07 +0300

    Rewrite (#2)
    * Remove old logic
    
    * Create new logic
    
    * Remove DataHolder
    
    * Fix time on page update, add start\pause text switch
    
    * Add notification, fix some issues

--- script.js
+++ script.js
@@ -2,44 +2,151 @@ const btnStart = document.getElementById('btnStart');
 const btnReset = document.getElementById('btnReset');
 const btnTimeIncrease = document.getElementById('btnTimeIncrease');
 const btnTimeDecrease = document.getElementById('btnTimeDecrease');
-
+const states = {none: 'none', work: 'work', pause: 'pause'};
+const props = {startTime: 'startTime', targetTime: 'targetTime', pauseTime: 'pauseTime', state: 'state', defaultTime: 'defaultTime'};
+const sourceTime = 25 * 60 * 1000;
+const timeLimits = {min: 1 * 60 * 1000, max: 99 * 60 * 1000};
+let timer;
 
 btnStart.addEventListener('click', function () {
-    toBack('start');
+    let state = getState();
+    if (state == states.none) {
+        onStart();
+    } else if (state == states.work) {
+        onPause();
+    } else if (state == states.pause) {
+        onResume();
+    }
 });
 
 btnReset.addEventListener('click', function () {
-    toBack('reset');
+    reset();
 });
 
 btnTimeIncrease.addEventListener('click', function () {
-    toBack('increase');
+    shiftDefaultTime(1000 * 60);
 });
 
 btnTimeDecrease.addEventListener('click', function () {
-    toBack('decrease');
+    shiftDefaultTime(-1000 * 60);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    //toBack('update');
+document.addEventListener('DOMContentLoaded', function() {
+    setupDefaultProperties();
+    updateTimerDisplay(getCurrentTime());
+    tryGetNotifyPermission();
 });
+
+function onStart() {
+    setState(states.work);
+    localStorage.setItem(props.startTime, Date.now());
+    localStorage.setItem(props.targetTime, getDefaultTime() + Date.now());
+    timer = setInterval(onUpdate, 1000);
+    btnStart.innerText = 'pause';
+    requestNotify(getCurrentTime());
+}
+
+function onUpdate() {
+    let currentTime = getCurrentTime();
+    if (currentTime < 500) {
+        reset();
+    }
+    updateTimerDisplay(getCurrentTime());
+}
+
+function onPause() {
+    setState(states.pause);
+    clearInterval(timer);
+    localStorage.setItem(props.pauseTime, Date.now());
+    btnStart.innerText = 'start';
+    requestCancelNotify();
+}
+
+function onResume() {
+    setState(states.work);
+    let pauseTime = parseInt(localStorage.getItem(props.pauseTime));
+    let targetTime = parseInt(localStorage.getItem(props.targetTime));
+    localStorage.setItem(props.targetTime, targetTime + Date.now() - pauseTime);
+    timer = setInterval(onUpdate, 1000);
+    btnStart.innerText = 'pause';
+    requestNotify(getCurrentTime());
+}
+
+function reset() {
+    if (timer) clearInterval(timer);
+    setState(states.none);
+    localStorage.removeItem(props.startTime);
+    localStorage.removeItem(props.targetTime);
+    localStorage.removeItem(props.pauseTime);
+    updateTimerDisplay(getCurrentTime());
+    requestCancelNotify();
+    btnStart.innerText = 'start';
+}
+
+function setState(state) {
+    localStorage.setItem(props.state, state);
+}
+
+function getState() {
+    let state = localStorage.getItem(props.state);
+    if (state) {
+        return state;
+    } else {
+        return states.none;
+    }
+}
 
 function updateTimerDisplay(time) {
-    document.getElementById('timerDisplay').innerText = 
-    `${(Math.floor(time/60/1000)).toString().padStart(2, '0')}:${(Math.floor(time/1000%60)).toString().padStart(2, '0')}`;
+    document.getElementById("timerDisplay").innerText = `${(Math.floor(time/1000/60)).toString().padStart(2, '0')}:${(Math.ceil(time/1000)%60).toString().padStart(2, '0')}`;
 }
 
-function toBack(message) {
-    navigator.serviceWorker.controller.postMessage({ action: message });
+function setupDefaultProperties() {
+    createUnsetProperty(props.defaultTime, sourceTime);
+    createUnsetProperty(props.state, states.none);
 }
 
-navigator.serviceWorker.addEventListener('message', function(event) {
-    const action = event.data.action;
-    if (action === 'timeUpdate') {
-        updateTimerDisplay(event.data.value);
-    } else if (action === 'start') {
-        btnStart.innerText = 'pause';
-    } else if (action === 'pause') {
-        btnStart.innerText = 'start';
+function createUnsetProperty(name, value) {
+    let property = localStorage.getItem(name);
+    if (!property) {
+        localStorage.setItem(name, value);
     }
-});
+}
+
+function getDefaultTime() {
+    return parseInt(localStorage.getItem(props.defaultTime));
+}
+
+function shiftDefaultTime(value) {
+    let newDefaultTime = getDefaultTime() + value;
+    if (newDefaultTime >= timeLimits.min && newDefaultTime <= timeLimits.max) {
+        localStorage.setItem(props.defaultTime, newDefaultTime);
+        updateTimerDisplay(getDefaultTime());
+    }
+}
+
+function getCurrentTime() {
+    let state = getState();
+    if (state == states.none) {
+        return getDefaultTime();
+    } else if (state == states.work) {
+        return (parseInt(localStorage.getItem(props.targetTime)) - Date.now());
+    } else if (state == states.pause) {
+        let pauseTime = parseInt(localStorage.getItem(props.pauseTime));
+        let targetTime = parseInt(localStorage.getItem(props.targetTime));
+        return targetTime - pauseTime;
+    }
+}
+
+function tryGetNotifyPermission() {
+    if (Notification.permission === 'default') {
+        Notification.requestPermission();
+    }
+}
+
+function requestNotify(time) {
+    navigator.serviceWorker.controller.postMessage({ action: 'notify', value: time });
+}
+
+function requestCancelNotify() {
+    navigator.serviceWorker.controller.postMessage({ action: 'cancelNotify' });
+}
--- sw.js
+++ sw.js
@@ -1,77 +1,38 @@
-importScripts('./timer.js');
+let notifyTimer;
 
-let defaultTime = 25;
-const timer = new Timer(defaultTime, 1000, onUpdate, onStart, onPause, onEnd);
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('cache-v1').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('message', function(event) {
-  const action = event.data.action;
-  if (action === 'start') {
-    if (timer.start()) {
-      console.log('Timer is start');
-    } else {
-      timer.pause();
-      console.log('Timer is paused');
-    }
-  } else if (action === 'reset') {
-    timer.reset();
-  } else if (action === 'increase') {
-    defaultTime = Math.min(defaultTime + 1, 99);
-    timer.reset(defaultTime);
-  } else if (action === 'decrease') {
-    defaultTime = Math.max(defaultTime - 1, 1);
-    timer.reset(defaultTime);
-  } else if (action === 'update') {
-    onUpdate();
-    if (timer.isWork()) {
-      onStart();
-    } else {
-      onPause();
-    }
-  }
-
-});
-
-function onUpdate() {
-  toFront({action: 'timeUpdate', value: timer.getTime()});
-}
-
-function onStart() {
-  toFront({action: 'start'});
-}
-
-function onPause() {
-  toFront({action: 'pause'});
-}
-
-function onEnd() {
-  showNotification();
-}
-
-function toFront(message) {
-  clients.matchAll().then(function(clients) {
-    clients.forEach(function(client) {
-      client.postMessage(message);
-    });
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('cache-v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html'
+        ]);
+      })
+    );
   });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  self.addEventListener('message', function(event) {
+    if (event.data.action == 'notify') {
+      scheduleNotification(event.data.value);
+    } else if (event.data.action == 'cancelNotify') {
+      if (notifyTimer) {
+        this.clearTimeout(notifyTimer);
+      }
+    }
+  });
+
+  function scheduleNotification(delay) {
+    notifyTimer = setTimeout(function() {
+      showNotification();
+    }, delay);
 }
 
 function showNotification() {
