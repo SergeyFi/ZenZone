3dcb704528d48de80d3ca4acd088dbec2f909954
SergeyFi <fearthunder@yandex.ru>
2024-07-27 20:29:57 +0300

    Store backgound url in localstorage

--- init.js
+++ init.js
@@ -2,3 +2,15 @@ import { App } from "./app.js";
 import { ZenTimer } from "./zenTimer.js";
 
 window.app = {self: new App(), timer: new ZenTimer()};
+
+document.addEventListener('DOMContentLoaded', function() {
+    initBackground();
+});
+
+function initBackground() {
+    if (!localStorage.getItem('resDefaultBackground')) {
+        localStorage.setItem('resDefaultBackground', 'https://studywithme.io/aesthetic-pomodoro-timer/c629a813b3561cfa40dd.jpg');
+    }
+    
+    document.body.style.backgroundImage = "url('" + localStorage.getItem('resDefaultBackground') + "')";
+}
